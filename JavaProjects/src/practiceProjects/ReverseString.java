065c69651e25e4cbb5a8a9ee972ecca2dba4df6d
Ahmad Habibzad <66021219+Habibzad@users.noreply.github.com>
2020-12-19 21:29:55 -0500

    Add files via upload

---
+++ JavaProjects/src/practiceProjects/ReverseString.java
@@ -0,0 +1,18 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String myReversedName = reverseName("Sarah");
+		System.out.println(myReversedName);
+	}
+
+	static String reverseName(String aName) {
+		String reversedName = "";
+		for(int i = aName.length()-1; i>=0; i--) {
+			reversedName += aName.charAt(i);
+		}
+		return reversedName;
+	}
+}
+
