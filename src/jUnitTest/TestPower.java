c4e08d66a173cc29c072d72cc7b89cc66bb90ecd
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-02-25 23:27:48 -0500

    Create JUnit Test for ReverseString method

--- src/jUnitTest/TestPower.java
+++ src/jUnitTest/TestPower.java
@@ -5,14 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import Projects.PowerFunction;
+import Projects.ReverseString;
 
 class TestPower {
-
+	
 	@Test
-	void test() {
+	public void testPowerFunction() {
 		PowerFunction func = new PowerFunction();
 		int result = func.pow(3, 3);
 		assertEquals(27, result);
+	}
+	
+	@Test
+	public void testReverseString() {
+		ReverseString rStr = new ReverseString();
+		String testReverse = rStr.reverseName("Hello");
+		assertEquals("olleH", testReverse);
 	}
 
 }
